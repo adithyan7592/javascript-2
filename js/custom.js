01a48aa92aa1d23e107edfec2f761bd6d48fd1a0
adithyan7592 <adhisreeji8@gmail.com>
2024-08-21 10:03:42 +0530

    repo new

---
+++ js/custom.js
@@ -0,0 +1,300 @@
+
+// let a="what is this"
+// let a=20 /redeclaration is not possible in let
+// var a="this is"
+// var a=2
+// console.log(a);
+// a=10
+// b=10
+// let c=20
+// c +=a;
+// console.log(c);
+// console.log(a+b);
+// console.log(a+c);
+// console.log(a==b&&b==c);
+// console.log(+"5");
+// console.log(5+ +"5");
+// console.log(a+=b);
+// console.log(!false);
+// console.log(a==b||a==c);
+// console.log(a*b);
+// mark=93
+// if(mark>90){
+//     console.log("A+");
+// }
+
+// else if(mark>70){
+//     console.log("A")
+// }
+// else if(mark>60){
+//     console.log("B+")
+// }
+// else if(mark>50){
+//     console.log("B")
+// }
+// else if(mark>40){
+//     console.log("C+")
+// }
+// else if(mark>30){
+//     console.log("FAILED");
+// }
+
+// score=prompt("enter your score")
+// if(score>90){
+//     alert('pass')
+// }
+// else{
+//     alert('fail')
+// }
+// fno=parseFloat(prompt("enter the first number"))
+// sno=parseFloat(prompt("enter the second number"))
+// alert(fno+sno)
+// op=6
+// switch(op){
+//     case 1:console.log("sunday");
+//     break;
+//     case 2:console.log("monday");
+//     break;
+//     case 3:console.log("tuesday");
+//     break;
+//     default:console.log("invalid");
+//     break;
+// }
+// i=1
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+// i=1
+// do{
+//     console.log(i);
+//     i=i+2;
+// }while(i<=10)
+//  for(i=1;i<=100;i++)
+//     {
+//     if(i%3==0){
+//         console.log("fiz");
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     }   
+// for(i=1;i<=100;i++)
+//     {
+//         if(i%5==0){
+//             console.log("buzz");
+//         }
+//         else{
+//             console.log(i)
+//         }
+//     }
+    // for(i=1;i<=100;i++)
+    //     {
+    //         if(i%5==0&&i%3==0){
+    //             console.log(" fizzbuzz");
+    //         }
+    //         else{
+    //             console.log(i)
+    //         }
+    //     }
+    // for(i=1;i<=100;i++)
+    //     {
+    //         if(i%5==0&&i%3==0){
+    //             console.log("fizzbuzz");
+    //         }
+    //         else if(i%3==0){
+    //             console.log("fizz");
+    //         }
+    //         else if(i%5==0){        
+    //             console.log("buzz");
+    //         }
+            
+            
+    //         else{
+    //             console.log(i);
+    //         }
+    //     }
+
+    // for(i=1;i<=10;i++){
+    //     if(i==5){
+    //     break;
+    //   }
+    // console.log(i);
+        
+    // }
+    // for(i=1;i<=10;i++){
+    //     if(i==5){
+    //     continue;
+    //     }
+    // console.log(i);
+        
+    // }
+    // function asd(){
+
+    //     if(true){
+    //         let a=10
+    //         console.log("inside block"+a);
+    //     }
+    //     console.log("inside function"+a);
+    // }
+    // asd()
+    // function asd(){
+
+    //     if(true){
+    //         var a=10
+    //         console.log("inside block"+a);
+    //     }
+    //     console.log("inside function"+a);
+    // }
+    // asd()
+    // function asd(){
+
+    //     if(true){
+    //         var a=10
+    //         console.log("inside block"+a);
+    //     }
+    //     console.log("inside function"+a);
+    // }
+    // asd()
+    // console.log("outside function"+a);
+    // for(i=1;i<=5;i++){
+    //     str=""
+    // for(j=1;j<=i;j++){
+    //     str+=j
+    // }
+    // console.log(str);
+    // }
+    // for(i=1;i<=5;i++){
+    //     str=""
+    // for(j=1;j<=i;j++){
+    //     str+="*"
+    // }
+    // console.log(str);
+    // }
+    // for(i=1;i<=5;i++){
+    //     str=""
+    // for(j=5;j>=1;j--){
+    //     str+=j
+    // }
+    // console.log(str);
+    // }
+    // for(i=5;i>=1;i--){
+    //     str=""
+    //     for(j=5;j>=i;j--){
+    //         str+=j
+    //     }
+    //     console.log(str);
+    // }
+    // for(i=1;i<=5;i++){
+    //     str=""
+    //     for(j=5;j>=i;j--){
+    //         str+=j
+    //     }
+    //     console.log(str);
+    // }
+    // for(i=5;i>=1;i--){
+    //     str=""
+    //     for(j=1;j<=i;j++){
+    //         str+=j
+    //     }
+    //     console.log(str);
+    // }
+    // num=1
+    // for(i=1;i<=4;i++){
+    //     str=""
+    //     for(j=0;j<i;j++){
+    //         str+=num
+    //         num++
+    //     }
+    //     console.log(str);
+    // }
+
+           //array
+    // let arr = ["raju",33,"dghjuky"]
+    // console.log(arr[0]);
+    // const arr =["raju",33,"dghjuky"]
+    // arr[0]=40
+    // console.log(arr[0]);
+
+      //object
+
+    //  let obj = {
+    //     name:"adhi",
+    //     age:20,
+    //     adress:{
+    //         houseno:40,
+    //         houseadress:"addddddd"
+    //     },
+    //     salary:400000,
+    //     hobby:"gaming"
+    // }
+    // obj["age"] = 34;
+    // console.log(obj.adress.houseno);
+    // console.log(obj["name"]);
+    // console.log(obj.age);
+
+    // let a = prompt("enter the number 1")
+    // let b = prompt("enter the number 2")
+    // let c = (Number(a)+Number(b))
+    // alert(c)
+
+//     let a = parseFloat(prompt("enter the number 1"))
+//     let b = parseFloat(prompt("enter the number 2"))
+//     let c = parseFloat(prompt("enter the number 3"))
+
+//     if(a>b && a>c)
+//       {
+//         alert("a is big")
+//  }
+//      else if (b>c && b>a){
+//         alert("b is big")
+        
+//     }
+//      else{
+//         alert("c is big")
+//      }
+   
+     
+   //switch program
+
+   // let day = parseFloat(prompt("enter the day"))
+   // switch (day){
+   //    case 1: alert("monday")
+   //    break;
+   //    case 2: alert("tuesday")
+   //    break;
+   //    case 3: alert("wednesday")
+   //    break;
+   //    case 4: alert("thursday")
+   //    break;
+   //    case 5: alert("friday")
+   //    break;
+
+   // }
+
+   //for of loop
+
+   // let numbers =[2,3,4,5,6,7,8]
+   // for(let n of numbers){
+   //    console.log(n);
+   // }
+
+   //for in loop
+   
+   // let obj = {
+   //    name:"adhi",
+   //    age:20,
+   //    hobby: "gaming",
+   // };
+   // for(let n in obj){
+   //    console.log(n);
+   // }
+
+   // let obj = {
+   //    name:"adhi",
+   //    age:20,
+   //    hobby: "gaming",
+   // };
+   // for(let n in obj){
+   //    console.log (obj[n]);
+   // }
